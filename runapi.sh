9c6d61a08f217ce6425bcb56427e449e87ba7fd8
Chad Cooper <chadcoop@me.com>
2024-09-11 17:33:53 -0400

    Update runapi.sh

--- runapi.sh
+++ runapi.sh
@@ -1,73 +1,42 @@
 #!/bin/bash
 
-# Description:
-# Test all commands or API's in a file or directory with this script.
-# This script extracts command from lines starting with ///// or ##### from the provided file(s).
-# It runs each command and prints the input command and output result one by one, optionally in markdown.
+get_curl_commands() {
+  
+    extract_lines() {
+        local file=$1
+        grep -E '^(#####|/////)[[:space:]]*curl' "$file" | sed 's/^[#\/]*[[:space:]]*//'
+    }
 
-# Prepare your files to use this script by adding ///// or ##### at start of a line.
-#   Example:
-#   /////
-#   ///// curl -X GET /api/endpoint
-#   ///// curl -X POST /api/endpoint
+    process_directory() {
+        local directory=$1
+        for file in "$directory"/*; do
+            if [ -d "$file" ]; then
+                process_directory "$file"
+            elif [ -f "$file" ]; then
+                extract_lines "$file"
+            fi
+        done
+    }
 
-# Usage: ./runapi.sh <file|directory> --markdown --responseType <type>
+    if [ -z "$1" ]; then
+        echo "Usage: $0 <directory>"
+        exit 1
+    fi
 
-# Flags:
-#   --markdown: Output results as file in markdown format.
-#   --responseType: Response format type (json, yaml, xml, html, text)
-
-api_md_file="api.md"
-
-check_usage_argument() {
-  # Check if a file or directory is provided as an argument
-  if [ -z "$1" ]; then
-    echo "Usage: $0 <file|directory> --markdown --responseType <type>"
-    exit 1
-  fi
-}
-
-parse_arguments() {
-  while [[ "$#" -gt 0 ]]; do
-    case $1 in
-      --markdown) markdown=true ;;
-      --responseType) responseType="$2"; shift ;;
-      *) path="$1" ;;
-    esac
-    shift
-  done
-}
-
-extract_lines() {
-  grep -E '^(/////|#####)' "$1" | sed -E 's/^(\/\/\/\/\/ *|##### *)//' | while IFS= read -r line; do
-    echo "$line"
-    echo "Reponse:"
-    echo '```'$responseType
-    eval "$line"
-    echo '```'
-    echo ""
-  done
-}
-
-process_file_or_directory() {
-  if [ -d "$path" ]; then
-    for file in "$path"/*; do
-      extract_lines "$file"
+    process_directory "$1" | while read -r line; do
+        echo "$line"
     done
-  else
-    extract_lines "$path"
-  fi
 }
 
-output_to_api_md() {
-  if [ "$markdown" = true ]; then
-    process_file_or_directory > "$api_md_file"
-    cat "$api_md_file"
-  else
-    process_file_or_directory
-  fi
+run_curl_commands() {
+    while read -r line; do
+        response=$(eval "$line" 2>/dev/null)
+        echo "Request:"
+        echo $line
+        echo "Response:"
+        echo $response
+        echo ""
+    done
 }
 
-check_usage_argument "$1"
-parse_arguments "$@"
-output_to_api_md
+get_curl_commands "$@" | run_curl_commands
